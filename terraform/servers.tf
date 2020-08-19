00104dd8ea94659bb8e74f667df587035ab95fa9
willtome <willtome@gmail.com>
2020-08-19 15:12:55 -0400

    update server size

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -17,7 +17,7 @@ data "aws_subnet_ids" "my_subnet" {
 
 resource "aws_instance" "k8s-master-1" {
     ami = "ami-08e923f2f38197e46"
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
     key_name = "wtome-lightbulb"
     vpc_security_group_ids = ["sg-030d834a"]
     subnet_id = tolist(data.aws_subnet_ids.my_subnet.ids)[0]
@@ -32,7 +32,7 @@ resource "aws_instance" "k8s-master-1" {
 
 resource "aws_instance" "k8s-master-2" {
     ami = "ami-08e923f2f38197e46"
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
     key_name = "wtome-lightbulb"
     vpc_security_group_ids = ["sg-030d834a"]
     subnet_id = tolist(data.aws_subnet_ids.my_subnet.ids)[0]
@@ -47,7 +47,7 @@ resource "aws_instance" "k8s-master-2" {
 
 resource "aws_instance" "k8s-master-3" {
     ami = "ami-08e923f2f38197e46"
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
     key_name = "wtome-lightbulb"
     vpc_security_group_ids = ["sg-030d834a"]
     subnet_id = tolist(data.aws_subnet_ids.my_subnet.ids)[0]
